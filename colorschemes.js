69051825f5ca3598dc10e00300f35681a207daa0
kuroese <kuroese@student.uzlabina.cz>
2021-04-14 01:30:35 +0200

    Initial commit

---
+++ colorschemes.js
@@ -0,0 +1,4 @@
+function changeScheme(primary, secondary) {
+	document.documentElement.style.setProperty("--primary", primary);
+	document.documentElement.style.setProperty("--secondary", secondary);
+}
